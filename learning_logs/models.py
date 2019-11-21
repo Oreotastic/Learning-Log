c05587b6c0cc069a94fe9fc689c6ebde8f9b7f19
Oretastic <hloreair@outlook.com>
2019-11-21 01:58:24 -0500

    made lot of changes for learning_logs

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# Add topic to learn
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
@@ -9,3 +11,19 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+# Add an Entry to a Topic.
+class Entry(models.Model):
+    """Something specifically learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        if len(self.text) > 50:
+            return self.text[:50] +  "..."
+        else:
+            return self.text
